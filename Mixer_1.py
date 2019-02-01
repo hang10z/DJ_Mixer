9affcb2feb9096e03e48c13a6e43719ce5de725a
Kevin Taylor <kevin@mooshiestudios.com>
2019-01-31 23:06:15 -0500

    Changed to Checkbox

--- Mixer_1.py
+++ Mixer_1.py
@@ -11,25 +11,24 @@ mixer = pygame.mixer
 mixer.init()
 
 
-def track_start():
-    track.play(loops = -1)
-
-
-def track_stop():
-    track.stop()
-
-
 def shutdown():
     track.stop()
     app.destroy()
 
 
+track_playing = IntVar()
+
+
+def track_toggle():
+    if track_playing.get() == 1:
+        track.play(loops = -1)
+    else:
+        track.stop()
+
+
 track = mixer.Sound(sound_file)
 
-start_button = Button(app, command = track_start, text = "Start")
-start_button.pack(side = LEFT)
-stop_button = Button(app, command = track_stop, text = "Stop")
-stop_button.pack(side = RIGHT)
+Checkbutton(app, variable = track_playing, command = track_toggle, text = 'START/STOP').pack()
 app.protocol("WM_DELETE_WINDOW", shutdown)
 app.mainloop()
 
