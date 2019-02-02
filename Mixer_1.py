31162c59074e678944f5ad1456d78f14abc2e639
Kevin Taylor <kevin@mooshiestudios.com>
2019-02-01 20:37:28 -0500

    Changed to Checkbox

--- Mixer_1.py
+++ Mixer_1.py
@@ -3,7 +3,6 @@ import pygame.mixer
 
 app = Tk()
 app.title("KEVINS MIX")
-app.geometry('250x100+200+100')
 
 sound_file = "50459_M_RED_Nephlimizer.wav"
 
@@ -16,9 +15,6 @@ def shutdown():
     app.destroy()
 
 
-track_playing = IntVar()
-
-
 def track_toggle():
     if track_playing.get() == 1:
         track.play(loops = -1)
@@ -26,9 +22,21 @@ def track_toggle():
         track.stop()
 
 
-track = mixer.Sound(sound_file)
+volume = DoubleVar()
 
-Checkbutton(app, variable = track_playing, command = track_toggle, text = 'START/STOP').pack()
+# The 'v' that is passed into this method is the volume float value from the slider.
+# Then set the value of volume to the set_volume() method which is built into pygame
+def change_volume(v):
+    track.set_volume(volume.get())
+
+
+track = mixer.Sound(sound_file)
+track_playing = IntVar()
+Checkbutton(app, variable = track_playing, command = track_toggle, text = sound_file).pack()
+
+volume_scale = Scale(app, variable = volume, from_ = 0.0, to = 1.0, resolution = .01, command = change_volume, label = 'Volume', orient = HORIZONTAL )
+volume_scale.pack(side = RIGHT)
+
 app.protocol("WM_DELETE_WINDOW", shutdown)
 app.mainloop()
 
